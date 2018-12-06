0d147a14f437281c5e0c7f68f95d9a3943a2da7c
Dennis Sam <denniarems@gmail.com>
2018-12-06 14:50:06 +0530

    Guessing Game Completed

---
+++  1.Guessing_game/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func main() {
+	var userInput int
+	var secretNumber int
+	var quit = false
+	var count = 3
+	rand.Seed(time.Now().Unix())
+	secretNumber = rand.Intn(10)
+	for !quit {
+		fmt.Println(count, " Life Left")
+		fmt.Printf("Enter the number ")
+		fmt.Scan(&userInput)
+		if userInput == secretNumber {
+			fmt.Println("You Won")
+			quit = true
+		} else if userInput > secretNumber {
+			fmt.Println("Too High..")
+			count--
+		} else if userInput < secretNumber {
+			fmt.Println("Too Low..")
+			count--
+		}
+		if count == 0 {
+			quit = true
+			fmt.Println("You Loss")
+		}
+	}
+}
