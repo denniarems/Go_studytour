0830524f947d96bb4f79091d6cf69fdf3b94490b
Dennis Sam <denniarems@gmail.com>
2018-12-08 18:03:21 +0530

    new Deck created

--- 2.Cards/deck.go
+++ 2.Cards/deck.go
@@ -4,8 +4,21 @@ import "fmt"
 
 type deck []string
 
+func newDeck() deck {
+	cards := deck{}
+	var cardSuits = []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	var cardValues = []string{"One", "Two", "Three", "Four"}
+
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+"  Of  "+suit)
+		}
+	}
+	return cards
+}
+
 func (d deck) print() {
-	for i, card := range d {
-		fmt.Println(i, card)
+	for _, card := range d {
+		fmt.Println(card)
 	}
 }
